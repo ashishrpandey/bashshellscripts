9342a050e9668c1fe5e7ef744d950364ab7c249e
Ashish Pandey <tech4abetterfuture@gmail.com>
2018-09-05 14:08:20 +0530

    Update 03ifelse.sh

--- 03ifelse.sh
+++ 03ifelse.sh
@@ -4,11 +4,11 @@ clear
 echo "Enter a number between 1 and 3:"
 read VALUE
 
-if [ "$VALUE" -eq "1" ] 2>/dev/null; then
+if [ "$VALUE" -eq "1" ]; then
 	echo "You entered #1"
-elif [ "$VALUE" -eq "2" ] 2>/dev/null; then
+elif [ "$VALUE" -eq "2" ]; then
 	echo "You successfully entered #2"
-elif [ "$VALUE" -eq "3" ] 2>/dev/null; then
+elif [ "$VALUE" -eq "3" ]; then
 	echo "You entered the 3rd number"
 else
 	echo "You didn't follow the directions!"
